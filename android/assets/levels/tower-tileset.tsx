f47c326c4d384eac3b6087480024d75da31470f1
Sean O'Neill <seansaysmeh@gmail.com>
2018-12-01 21:03:33 +0000

    fixzed tiled artifact

--- android/assets/levels/tower-tileset.tsx
+++ android/assets/levels/tower-tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tower" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="tower-tileset.png" width="320" height="320"/>
+<tileset name="tower" tilewidth="32" tileheight="32" spacing="1" tilecount="150" columns="10">
+ <image source="tower-tileset.png" width="360" height="480"/>
 </tileset>
