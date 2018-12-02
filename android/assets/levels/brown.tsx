5f5570107df776a269d020139541e9e0064d4fe4
Sean O'Neill <seansaysmeh@gmail.com>
2018-12-02 14:29:29 +0000

    updated the second area tileset colors

--- android/assets/levels/brown.tsx
+++ android/assets/levels/brown.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="brown" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="tower-tileset-brown.png" width="320" height="320"/>
+<tileset name="brown" tilewidth="32" tileheight="32" spacing="1" tilecount="165" columns="10">
+ <image source="tower-tileset-brown.png" width="360" height="480"/>
 </tileset>
